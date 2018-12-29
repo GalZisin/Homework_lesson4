ff9b6d8523698e01c9e5afef760f0868bc049fbd
GalZisin <46171488+GalZisin@users.noreply.github.com>
2018-12-29 21:56:35 +0200

    Add files via upload

---
+++ Homework_lesson4.7/Homework_lesson4.7/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_lesson4._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0, ans = 0;
+            Console.WriteLine("Enter the first number a: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number b: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            while(i < b)
+            {
+                ans = ans + a;
+                i++;
+            }
+            Console.WriteLine("The answer is: "+ans);
+            Console.ReadLine();
+        }
+    }
+}
