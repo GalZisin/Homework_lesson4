831c68c802e0a7375dad8fdc4c986df330d7969d
GalZisin <46171488+GalZisin@users.noreply.github.com>
2018-12-29 21:49:24 +0200

    Add files via upload

---
+++ Homework_lesson4.3/Homework_lesson4.3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_lesson4._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         int x, sum = 0; ; 
+         
+         Console.WriteLine("Enter numbers and sum all until negative number is entered");
+         Console.WriteLine("----------------------------------------------------------");
+            Console.WriteLine("Enter anumber: ");
+            x = Convert.ToInt32(Console.ReadLine());
+            if (x > 0 || x == 0)
+            {
+                sum = x;
+            }
+            while (x > 0||x== 0){
+            Console.WriteLine("Enter anumber: ");
+            x = Convert.ToInt32(Console.ReadLine());
+                if(x > 0)
+                {
+                    sum = sum + x;
+                }
+           
+         }
+         Console.WriteLine("The sum of the numbers is: " + sum);
+         Console.ReadLine();
+        }
+
+
+    }
+}
