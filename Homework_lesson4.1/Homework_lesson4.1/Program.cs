e170f2c2d0e419c94e4a1c4cfcbb4e626304c763
GalZisin <46171488+GalZisin@users.noreply.github.com>
2018-12-29 21:44:29 +0200

    Add files via upload

---
+++ Homework_lesson4.1/Homework_lesson4.1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_lesson4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("Is a divide by b?");
+
+            Console.WriteLine("Enter a first number");
+
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter a second number");
+
+            b = Convert.ToInt32(Console.ReadLine());
+
+            if (a % b == 0)
+            {
+                Console.WriteLine("a divide by b ");
+            }
+            else
+            {
+                Console.WriteLine("a isn't divide by b ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
