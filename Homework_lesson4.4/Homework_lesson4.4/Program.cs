82d7ea77f749070c2ce5b11b5689f129f60f4302
GalZisin <46171488+GalZisin@users.noreply.github.com>
2018-12-29 21:50:00 +0200

    Add files via upload

---
+++ Homework_lesson4.4/Homework_lesson4.4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_lesson4._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int m = 2;
+            Console.WriteLine("Check if the numer is primery or not");
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("Enter a number: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            while(a % m!=0 && m < a)
+            {
+                m++;
+            }
+            if (m==a)
+            {
+                Console.WriteLine("The number is primery");
+            }
+                
+            else
+            {
+                Console.WriteLine("The numer isn't primery");
+            }
+            Console.ReadLine();
+        }
+    }
+}
