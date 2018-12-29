69ab39972f6d12792db3390bc0b2426f7c792223
GalZisin <46171488+GalZisin@users.noreply.github.com>
2018-12-29 21:55:47 +0200

    Add files via upload

---
+++ Homework_lesson4.6/Homework_lesson4.6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_lesson4._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int azeret = 1, i = 1;
+            Console.WriteLine("Enter a number for azeret culculaion");
+            int a = Convert.ToInt32(Console.ReadLine());
+            while (i < a|| i==a)
+            {
+                azeret = azeret * i;
+                i++;
+            }
+            Console.WriteLine("The result is :"+azeret);
+            Console.ReadLine();
+        }
+    }
+}
